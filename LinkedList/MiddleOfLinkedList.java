cfdda57819ccc7c50121f1c11256bbdf7428177a
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-10-03 20:42:18 +0530

    Update MiddleOfLinkedList.java
    problem link : https://leetcode.com/problems/middle-of-the-linked-list/

--- LinkedList/MiddleOfLinkedList.java
+++ LinkedList/MiddleOfLinkedList.java
@@ -5,7 +5,6 @@ class Solution {
         while(fast!=null && fast.next!=null){
             fast = fast.next.next;
             slow = slow.next;
-            
         }
         return slow;
     }
