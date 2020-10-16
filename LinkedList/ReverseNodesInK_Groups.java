6e9081ecd3ef8f600a0e001eeff46a81a6e17433
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-10-16 13:03:51 +0530

    changed the ff.next to arr[1].next in line 15

--- LinkedList/ReverseNodesInK_Groups.java
+++ LinkedList/ReverseNodesInK_Groups.java
@@ -3,16 +3,16 @@ class Solution {
         if(k==1 || head==null)return head;
         ListNode ff_prev = null,ff=head,ss_prev=head,ss=null;
         while(true){
-           int z = ss==null ? k-1 :k; 
+           int z = ss==null ? k-1 :k;  // if ss==null then we are doing reversal for first time , so z=k-1;
             while(ss_prev!=null && z-->0)
                 ss_prev = ss_prev.next;
             if(ss_prev==null)break;
             ss=ss_prev.next;
             ss_prev.next = null;
             ListNode[] arr = reverse(ff);
-            if(ff_prev==null)head=arr[0];
+            if(ff_prev==null)head=arr[0];   //first time reversal take place
             else ff_prev.next  = arr[0];
-            ff.next = ss;
+            arr[1].next = ss;
             ss_prev = arr[1];
             ff_prev = ss_prev;
             ff=ss;
