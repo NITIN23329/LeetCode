7acf4637263d404a65f0e69115d2a889c62017f6
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-10-16 13:01:34 +0530

    Create ReverseNodesInK_Groups.java

---
+++ LinkedList/ReverseNodesInK_Groups.java
@@ -0,0 +1,33 @@
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(k==1 || head==null)return head;
+        ListNode ff_prev = null,ff=head,ss_prev=head,ss=null;
+        while(true){
+           int z = ss==null ? k-1 :k; 
+            while(ss_prev!=null && z-->0)
+                ss_prev = ss_prev.next;
+            if(ss_prev==null)break;
+            ss=ss_prev.next;
+            ss_prev.next = null;
+            ListNode[] arr = reverse(ff);
+            if(ff_prev==null)head=arr[0];
+            else ff_prev.next  = arr[0];
+            ff.next = ss;
+            ss_prev = arr[1];
+            ff_prev = ss_prev;
+            ff=ss;
+        }
+        return head;
+    }
+    private ListNode[] reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr!=null){
+            ListNode fut = curr.next;
+            curr.next = prev;
+            prev=curr;
+            curr = fut;
+        }
+        return new ListNode[]{prev , head};
+    }
+}
