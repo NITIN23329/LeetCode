0aa5f70d1730b65379daae5d513cf8b5a23300eb
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-10-20 00:31:59 +0530

    Create ValidParentheses.java

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c==')' ){
+                if(st.isEmpty() || st.peek()!='(')return false;
+                st.pop();
+            }
+            else if(c==']' ){
+                if(st.isEmpty() || st.peek()!='[')return false;
+                st.pop();
+            }
+            else if(c=='}' ){
+                if(st.isEmpty() || st.peek()!='{')return false;
+                st.pop();
+            }
+            else st.push(c);
+        }
+        return st.isEmpty();
+    }
+}
