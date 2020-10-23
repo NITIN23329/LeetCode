0de62660f4a96d2637d7fcb137e1d7e3aebb81e6
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-10-24 00:49:04 +0530

    Added a faster solution

--- Stack/FlattenNestedListIterator.java
+++ Stack/FlattenNestedListIterator.java
@@ -1,3 +1,4 @@
+//approach 1 : 4ms time
 public class NestedIterator implements Iterator<Integer> {
 
     private Deque<NestedInteger> dq;
@@ -20,6 +21,36 @@ public class NestedIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
+        return !dq.isEmpty();
+    }
+}
+
+//approach 2 : 2ms time
+public class NestedIterator implements Iterator<Integer> {
+
+    private Deque<NestedInteger> dq;
+    public NestedIterator(List<NestedInteger> nestedList) {
+        dq = new ArrayDeque<>();
+        adder(nestedList);
+        
+    }
+    private void adder(List<NestedInteger> list){
+        for(int i = list.size()-1;i>=0;i--){
+           dq.push(list.get(i));
+        }
+    }
+    @Override
+    public Integer next() {
+       return dq.pop().getInteger();
+    }
+    //adder method is called in hasNext() and not in next() cuz consider test cas [[],[[[]]]] ,
+    //here no integer but if we call adder in next then we have to return a integer which is not possible.
+    @Override
+    public boolean hasNext() {
+       while(!dq.isEmpty() && !dq.peek().isInteger()){
+           List<NestedInteger> l = dq.pop().getList();
+           adder(l);
+       }
         return !dq.isEmpty();
     }
 }
