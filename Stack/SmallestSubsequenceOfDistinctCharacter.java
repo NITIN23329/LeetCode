ba9ff39b0285703ab5fbaf314f7e6a785535dd40
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-10-25 21:02:43 +0530

    Added loop invarient

--- Stack/SmallestSubsequenceOfDistinctCharacter.java
+++ Stack/SmallestSubsequenceOfDistinctCharacter.java
@@ -1,4 +1,5 @@
 //time O(n) , constant space O(26) as english alphabets are 26 only
+//loop invarient :  for every i , we will have smallest distinct subsequence for all discovered characters till that i
 class Solution {
     public String smallestSubsequence(String s) {
         Map<Character,Integer> map = new HashMap<>(); //it will store last index of each character
@@ -11,7 +12,8 @@ class Solution {
             while(!dq.isEmpty()){
                 if(map.get(dq.peek())<i)break;    // if the top element in dq will not occur again future, then we can not remove it from dq
                 if(dq.peek()<arr[i])break;        //if top element of dq is smaller than current , then don't remove it as it is lexiographically ...
-                set.remove(dq.pop());             //small than adding curr and then top dq element(smaller than curr element).
+                                                  //small than adding curr and then top dq element(smaller than curr element).
+                  set.remove(dq.pop());  
             }
             dq.push(arr[i]);
             set.add(arr[i]);
