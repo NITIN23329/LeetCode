8d14aa7061e420b0bc5ea9ab6efdfcd1298e22f7
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-10-26 03:07:27 +0530

    added more comments

--- Stack/DecodeString.java
+++ Stack/DecodeString.java
@@ -3,10 +3,10 @@ class Solution {
         Deque<String> dq = new ArrayDeque<>();  //stack
         StringBuilder num = new StringBuilder();
         StringBuilder str = new StringBuilder();
-        s = "1["+s+']';   //handle base corner case.
+        s = "1["+s+']';   //handle  corner case like "abc3[de]2[fg]"
         for(char c:s.toCharArray()){
             if(c=='['){
-                if(!str.toString().isEmpty()){      //cases like ...2[z]abc3[deg]...here "abc" has no prefix integer. 
+                if(!str.toString().isEmpty()){      //cases like "...2[z]abc3[deg]..." ,here "abc" has no prefix integer. 
                     dq.push(str.toString());        //Here we are also adding "abc" to dq
                     str = new StringBuilder();
                 }
@@ -15,7 +15,7 @@ class Solution {
             }
             else if(c==']'){
                 while(!dq.isEmpty() && !(dq.peek().charAt(0) >='0' && dq.peek().charAt(0) <='9'))
-                    str.insert(0, dq.pop());            //handle case when there are more than 1 consecutive strings in dq.
+                    str.insert(0, dq.pop());            //handle case when there are more than 1 consecutive strings in dq like "2[3[a]2[bc]]".
                 int f = dq.isEmpty()? 1:Integer.parseInt(dq.pop());
                 StringBuilder t = new StringBuilder();
                 for(int i=0;i<f;i++)t.append(str);
