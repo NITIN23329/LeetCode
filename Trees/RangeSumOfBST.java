aacf50cf362a85514be2893a24955c618c9b5112
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-11-11 19:28:25 +0530

    Create RangeSumOfBST.java

---
+++ Trees/RangeSumOfBST.java
@@ -0,0 +1,13 @@
+//time O(N) , N = no. of nodes : space : O(height)
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null)return 0;
+        if(root.val>=low && root.val<=high){
+            return root.val + rangeSumBST(root.left , low , high) + 
+                rangeSumBST(root.right,low,high);
+        }
+         if( root.val < low)
+            return rangeSumBST(root.right,low,high);
+         return rangeSumBST(root.left , low,high);
+    }
+}
