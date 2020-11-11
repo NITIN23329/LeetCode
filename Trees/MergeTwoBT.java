409ab51aeedb5e8c44ff8d350a4debb60c4d911e
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-11-11 19:35:13 +0530

    Create MergeTwoBT.java

---
+++ Trees/MergeTwoBT.java
@@ -0,0 +1,10 @@
+    //time O(n) , n= no. of nodes in t1 : space O(height of t1)
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1==null)return t2;
+        if(t2==null)return t1;
+        t1.val+=t2.val;
+        t1.left = mergeTrees(t1.left,t2.left);
+        t1.right = mergeTrees(t1.right,t2.right);
+        return t1;
+    }
+}
