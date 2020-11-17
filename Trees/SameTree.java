8202fd6a1515c49d89121f0c5c8a31fba3c63ae7
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-11-17 11:29:35 +0530

    Create SameTree.java

---
+++ Trees/SameTree.java
@@ -0,0 +1,8 @@
+//time O(N) , space O(height)
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)return true;
+        if(p==null || q==null)return false;
+        return p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
