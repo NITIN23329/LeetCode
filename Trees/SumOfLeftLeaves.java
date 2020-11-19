c04fd0276d0c797b9e47ebbe32b87b3322428a96
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-11-19 19:16:06 +0530

    Create SumOfLeftLeaves.java

---
+++ Trees/SumOfLeftLeaves.java
@@ -0,0 +1,10 @@
+//recursive time O(N) , space O(height)
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null)return 0;
+        if(root.left!=null &&
+            root.left.left==null && root.left.right==null)
+            return root.left.val+sumOfLeftLeaves(root.right);
+        else return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+    }
+}
