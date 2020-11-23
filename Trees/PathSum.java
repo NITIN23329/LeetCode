021a7f62e9cc5dddf998b1f8c41338e08070a3ae
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-11-23 13:44:26 +0530

    Create PathSum.java

---
+++ Trees/PathSum.java
@@ -0,0 +1,12 @@
+//recursive 
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null)return false;
+        sum-=root.val;
+        if(root.left==null && root.right==null){
+            if(sum==0)return true;
+            else return false;
+        }
+        return hasPathSum(root.left,sum) || hasPathSum(root.right,sum);
+    }
+}
