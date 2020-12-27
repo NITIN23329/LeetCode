1721461e4babc33cdddbee2e8dcb16a7a45c4638
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2020-12-27 11:09:35 +0530

    Update IncreasionOrderSearchTree.java

--- Trees/IncreasionOrderSearchTree.java
+++ Trees/IncreasionOrderSearchTree.java
@@ -19,3 +19,35 @@ class Solution {
         inOrder(x.right);
     }
 }
+//time O(n) , space O(height)
+// keep hold of previously visited node, while doing inorder, update the refrence of previous node and make current node as previous and goto next node.
+// one thing is to make the left and right of last visited node be null cuz of case : it will create a cycle if not done so.
+/*                                                                              2
+                                                                               / \
+                                                                              1   5
+                                                                                 /
+                                                                                4
+                                                                               /
+                                                                              3
+*/
+class Solution {
+    TreeNode prev;
+    TreeNode nr;
+    public TreeNode increasingBST(TreeNode root) {
+        prev = null;nr=null;
+        inOrder(root);
+        prev.left=null;
+        return nr;
+        
+    }
+    private void inOrder(TreeNode root){
+        if(root==null)return;
+        inOrder(root.left);
+        if(prev!=null){
+            prev.right = root;
+            prev.left = null;
+        }else nr=root;
+        prev = root;
+        inOrder(root.right);
+    }
+}
