0568bdfe929d152bb4778867e8b20e96711573ad
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-01-01 10:35:39 +0530

    added some more explanation

--- Heap/KthLargestEmenetsInAStream.java
+++ Heap/KthLargestEmenetsInAStream.java
@@ -1,7 +1,7 @@
 // time complexity of add() is O(logk) , constructor() is O(nlogk)
 /*  approach :
       --> maintain a min heap of size k. 
-      --> min heap contains largest k elements out of n
+      --> min heap contains largest k elements out of n, so that kth largest element is the top element.
       --> todo so , if min heap size==k , and if the incoming element is , top , we do nothing as kth largest sill remains same (top element)
       --> if the incomming element is > top , then we remove top and add the incoming element.
 */
