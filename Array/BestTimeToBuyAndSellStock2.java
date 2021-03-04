7f29cb24ab6f1e2ea45fc18ffcce4304e78fe0bf
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-03-04 22:11:15 +0530

    added FSM Solution

--- Array/BestTimeToBuyAndSellStock2.java
+++ Array/BestTimeToBuyAndSellStock2.java
@@ -9,3 +9,21 @@ class Solution {
         return ans;
     }
 }
+// time and space complexity : O(n) , although, we can reduce space complexity to O(1)
+// this solution is done using FSM , FSM : https://user-images.githubusercontent.com/55681638/109997644-6ae48800-7d36-11eb-88a6-b4eab7a91032.jpg
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] buy = new int[n];
+        int[] sell = new int[n];
+        int[] nothing = new int[n];
+        buy[0] = -prices[0];
+        for(int day=1;day<n;day++){
+            buy[day] =Math.max(buy[day-1],Math.max(nothing[day-1]-prices[day],sell[day-1]-prices[day]));
+            sell[day] = Math.max(sell[day-1] , buy[day-1] + prices[day]);
+            nothing[day] = Math.max(nothing[day-1] , sell[day-1]);
+        }
+        return sell[n-1];
+    }
+}
