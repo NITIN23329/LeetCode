5f57582f65bdbced0671e8108d377c3760eda4a0
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-03-06 00:13:13 +0530

    added buttom up dp

--- dynamicProgramming/ClimbingStairs.java
+++ dynamicProgramming/ClimbingStairs.java
@@ -10,7 +10,7 @@ class Solution {
         return recursive(n-1) + recursive(n-2);
     }
 }
-// time complexity O(n)
+// time and complexity O(n)
 // memoization of above recursive code.
 class Solution {
     public int climbStairs(int n) {
@@ -23,5 +23,17 @@ class Solution {
         if(n<0)return 0;
         if(dp[n]!=-1)return dp[n];
         return dp[n] = memoHelper(n-1,dp) + memoHelper(n-2,dp);
+    }
+}
+// time and space complexity O(1) , however space complexity ccan be reduced to O(1)
+// bottom up dp
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[0] = 1;
+        for(int i=2;i<=n;i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 }
