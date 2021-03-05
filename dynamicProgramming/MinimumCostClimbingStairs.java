d706701e64fb02a566ba96a5b9edf40ac1387ca0
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-03-06 00:21:37 +0530

    added bottom up dp

--- dynamicProgramming/MinimumCostClimbingStairs.java
+++ dynamicProgramming/MinimumCostClimbingStairs.java
@@ -1,4 +1,4 @@
-// time compexity O(n)
+// time and space compexity O(n)
 /*  approach :
       --> if we are at ith index  , 
           --> we can either pay from cost[i] and goto next indeces
@@ -18,4 +18,17 @@ class Solution {
         return dp[i] = Math.min(s0,s1);
     }
     
+}
+// time and space complexity :O(n) , however space complexity can be reduce to O(1) 
+// bottom up dp
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int [] dp = new int[n+1];
+        dp[0] = cost[0];
+        dp[1]= cost[1];
+        for(int i=2;i<=n;i++)
+            dp[i] = Math.min(dp[i-1],dp[i-2]) + (i==n? 0 : cost[i]);
+        return dp[n];
+    }
 }
