f308e6c16bcee447f83b873bd4ce7f008f0787dc
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-07-05 14:19:44 +0530

    added some comments

--- Stack/132Pattern.java
+++ Stack/132Pattern.java
@@ -3,35 +3,14 @@
 *using stack, for each i,
 find the smallest element for each i on right side.,which is greater than left[i] .
 *correctness of this approaach:
-1)left[] array is a decreasing array left[i]>=left[j] , i<j.
-2)  for finding smaller on right we are traversing from right to left, 
-so , for each j , if there is a value on right which is smaller than left[j] ,we will pop it.(as it is automatically smaller than left[i].
+* left[] array is a increasing array left[i]<=left[j] , i<j.
+* Suppose we are at index i and we found a element x>left[i] to right of i. Then 2 cases are possible,
+    if x<nums[i] then we found a valid combination,
+    if x > nums[i] and if there is another element on the right of x which < nums[i](which we can not see at index i) then left[i],
+        that element and x will be a valid combination when we are processing index of x.
 */
-class Solution {
-    public boolean find132pattern(int[] nums) {
-        int n = nums.length;
-        int left[] = new int[n];
-        int min = Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            left[i] = min;
-            min = Math.min(min,nums[i]);
-        }
-        Deque<Integer> dq = new ArrayDeque<>();
-        int[] right = new int[n];
-        for(int i=n-1;i>=0;i--){
-            while(!dq.isEmpty() && left[i]>=dq.peek())
-                dq.pop();
-            right[i] = dq.isEmpty()? Integer.MAX_VALUE :dq.peek();
-            dq.push(nums[i]);
-        }
-        for(int i=0;i<n;i++){
-            if(left[i]<right[i] && right[i]<nums[i])
-                return true;
-        }
-        return false;
-    }
-}
-// optimization of above problem : 2 pass solution
+
+// 2 pass stack solution
 class Solution {
     public boolean find132pattern(int[] nums) {
         int n = nums.length;
