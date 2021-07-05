6e4ea458a2e2b716509622725b4479945b36d59b
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-07-05 18:46:40 +0530

    fixed a bug in last solution

--- Stack/SumOfSubarrayMinimum.java
+++ Stack/SumOfSubarrayMinimum.java
@@ -46,34 +46,33 @@ class Solution {
 1) use a stack to find no. of elements in between current element and smaller element(than current) on both left and right side. (Save them in array)
 2) after finding above numbers , we will find number of subrrays for which current element is minimum (no of elements on left*no of elements on right).
 3) after finding no. of such subrrays multiply  it with value of current element to get sum of minimum of all subarrays with current element as its minimum.
-Note: I know this solution is not intuitive...I also didn't think of this solution on first place.I came up with O(n^2) solution,
-	  then I took hints from youtube videos. (Not the code but some thinking).So its fine if you aren't able to solve this problem in first go. 
-	  If  you still not able to get it , dry run below code and you will able to it.
+Note: In case of repetitive values, consider them only at one side. example [3,2,5,2]. 
+	For 2 at index 1, leftCount[1] = 2 and rightCount[1] = 4. and for leftCount[3] = 2 and rightCount[3] = 1
+	So we are gonna pop them only at one side otherwise we will create duplicate subarray.
  */
 class Solution {
 	public int sumSubarrayMins(int[] arr) {
-		int n = arr.length;
-		int[] leftMin = new int[n]; //store no.  elements between current element and left smaller element.
-		int[] rightMin = new int[n];//store no.  elements between current element and right smaller element.
-		int mod = (int)1e9+7;
-		Deque<Integer> ind = new ArrayDeque<>();
-		for(int i=0;i<n;i++){
-			while(!ind.isEmpty() && arr[ind.peek()]>arr[i])
-				ind.pop();
-			leftMin[i] = ind.isEmpty()? i+1:i-ind.peek();
-			ind.push(i);
-		}
-		ind = new ArrayDeque<>();
-		for(int i = n-1;i>=0;i--){
-			while(!ind.isEmpty() && arr[ind.peek()]>=arr[i])
-				ind.pop();
-			rightMin[i] = ind.isEmpty()? n-i : ind.peek()-i;
-			ind.push(i);
-		}
-		int res = 0;
-		for(int i=0;i<n;i++)
-			res = (res+arr[i]*leftMin[i]*rightMin[i])%mod;    //finding sum of minimum of all possible subarrays with current element as minimum.
-		return res;
-
-	}
+        int n = arr.length;
+        long[] leftCount = new long[n];
+        long[] rightCount = new long[n];
+        Deque<Integer> dq = new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            while(!dq.isEmpty() && arr[dq.peek()] >= arr[i])dq.pop();	// pop same elements
+            leftCount[i] = dq.isEmpty()?i+1: i - dq.peek();
+            dq.push(i);
+        }
+        dq.clear();
+        for(int i=n-1;i>=0;i--){
+            while(!dq.isEmpty() && arr[dq.peek()] > arr[i])dq.pop();	// don't pop same elements
+            rightCount[i] = dq.isEmpty()?n-i:dq.peek() - i;
+            dq.push(i);
+        }
+        long ans = 0;
+        int mod = (int)1e9+7;
+        for(int i=0;i<n;i++){
+            ans += (leftCount[i]*rightCount[i]*arr[i]);
+            ans %=mod;
+        }
+        return (int)ans;
+    }
 }
