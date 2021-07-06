1ec713d8cc001fbc862a71cb68940d503b24413d
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-07-06 12:41:42 +0530

    added more shorter code

--- Queue/SlidingWindowMaximum.java
+++ Queue/SlidingWindowMaximum.java
@@ -6,18 +6,13 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<int[]> dq = new ArrayDeque<>();
         int n = nums.length;
-        int[] arr = new int[n-k+1];
-        for(int i=0;i<k;i++){
-            while(!dq.isEmpty() && dq.peekLast()[0]<nums[i])dq.pollLast();
+        int[] res = new int[n-k+1];
+        for(int i=0;i<n;i++){
+            while(!dq.isEmpty() && dq.peekLast()[0]<=nums[i])dq.removeLast();
             dq.addLast(new int[]{nums[i],i});
+            if(dq.peekFirst()[1]==i-k)dq.removeFirst();
+            if(i>=k-1)res[i-k+1] = dq.peekFirst()[0];
         }
-        arr[0]  = dq.peekFirst()[0];
-        for(int i=k;i<n;i++){
-            if(!dq.isEmpty() && dq.peekFirst()[1]<=i-k)dq.pollFirst();
-            while(!dq.isEmpty() && dq.peekLast()[0]<nums[i])dq.pollLast();
-            dq.addLast(new int[]{nums[i],i});
-            arr[i-k+1] = dq.peekFirst()[0];
-        } 
-        return arr;
+        return res;
     }
 }
